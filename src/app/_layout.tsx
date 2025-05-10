a94b728328378e542f9913f95c37104981eb7ce7
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-05-10 17:38:46 -0300

    add customized configuration to Drawer navigation

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,37 @@
+import { MaterialIcons } from '@expo/vector-icons'
 import { Drawer } from 'expo-router/drawer'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer />
+      <Drawer
+        screenOptions={{
+          drawerActiveTintColor: '#000000',
+          drawerInactiveTintColor: '#999999',
+        }}
+      >
+        <Drawer.Screen
+          name="index"
+          options={{
+            drawerLabel: 'Entrar',
+            title: 'Entrar',
+            drawerIcon: ({ size, color }) => (
+              <MaterialIcons name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="sign-up"
+          options={{
+            drawerLabel: 'Criar conta',
+            title: 'Criar conta',
+            drawerIcon: ({ size, color }) => (
+              <MaterialIcons name="home" size={size} color={color} />
+            ),
+          }}
+        />
+      </Drawer>
     </GestureHandlerRootView>
   )
 }
