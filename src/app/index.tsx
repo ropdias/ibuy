9638369f81e5fefeea5de4c83b201cc4c310aca4
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-05-10 15:44:44 -0300

    use asChild in Link component to create a button inside it

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,13 @@
 import { Link } from 'expo-router'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Link href="/sign-up" style={styles.new}>
-        Criar conta
+      <Link href="/sign-up" asChild>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.label}>Criar conta</Text>
+        </TouchableOpacity>
       </Link>
     </View>
   )
@@ -17,8 +19,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  new: {
+  label: {
     fontSize: 16,
     fontWeight: 'bold',
+    color: '#FFF',
+  },
+  button: {
+    backgroundColor: '#000',
+    paddingHorizontal: 32,
+    paddingVertical: 10,
+    borderRadius: 10,
   },
 })
