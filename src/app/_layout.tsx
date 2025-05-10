8a7f64e625954f7f65ca9950be41e0a1277a23b5
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-05-10 17:31:21 -0300

    add customized configuration to Tabs navigation

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,34 @@
+import { MaterialIcons } from '@expo/vector-icons'
 import { Tabs } from 'expo-router'
 
 export default function Layout() {
-  return <Tabs screenOptions={{ headerShown: false }} />
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarInactiveTintColor: '#CECECE',
+        tabBarActiveTintColor: '#000',
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: 'Sign In',
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="home" size={20} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="sign-up"
+        options={{
+          tabBarLabel: 'Sign Up',
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="person" size={20} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  )
 }
